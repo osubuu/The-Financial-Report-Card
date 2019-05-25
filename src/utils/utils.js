02b0c13e4ba8f80afe242b5f57c2be993d05d91a
Khoi <hakhoipham@gmail.com>
2019-05-25 11:10:48 -0400

    started refactoring

---
+++ src/utils/utils.js
@@ -0,0 +1,17 @@
+import _ from "lodash";
+
+const Utils = {
+  getRandomUniqueNumbers: (amountOfUniqueNumbers, range) => {
+    const uniqueNumbers = [];
+    while (uniqueNumbers.length < amountOfUniqueNumbers) {
+      const randomNumber = _.random(range);
+      // make sure each numbers are unique
+      if (!_.includes(uniqueNumbers, randomNumber)) {
+        uniqueNumbers.push(randomNumber);
+      }
+    }
+    return uniqueNumbers;
+  }
+}
+
+export default Utils;
