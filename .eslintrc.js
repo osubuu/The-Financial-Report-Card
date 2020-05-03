177c83a6af7c0a93085b253cb34abbff703ddd39
Khoi <hakhoipham@gmail.com>
2020-05-02 20:36:01 -0400

    eslint updated for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,37 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": "airbnb",
-    "parser": "babel-eslint",
-    "rules": {
-        "indent": [2, "tab", { "SwitchCase": 1, "VariableDeclarator": 1 }],
-        "no-tabs": 0,
-        "react/prop-types": 0,
-        "react/jsx-indent": [2, "tab"],
-        "react/jsx-indent-props": [2, "tab"],
-        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-        "import/no-extraneous-dependencies": "off",
-        "arrow-params": "as-needed",
-    }
-}
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'indent': [2, 'tab', { 'SwitchCase': 1, 'VariableDeclarator': 1 }],
+    'no-tabs': 0,
+    'react/prop-types': 0,
+    'react/jsx-indent': [2, 'tab'],
+    'react/jsx-indent-props': [2, 'tab'],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    'quotes': [2, 'single', 'avoid-escape'],
+    // 'arrow-params': 'as-needed',
+  },
+};
