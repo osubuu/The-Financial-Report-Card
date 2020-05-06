732491a6c7421d2cd93854e3dff585fe532b9a0f
Khoi <hakhoipham@gmail.com>
2020-05-05 23:16:23 -0400

    date updated

--- src/views/homepage/components/Copyright.tsx
+++ src/views/homepage/components/Copyright.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Copyright = (): JSX.Element => (
 	<div className="copyright-container">
 		<h6 className="copyright">
-			&#169; 2019 Khoi Pham ---
+			&#169; 2020 Khoi Pham ---
 			{' '}
 			<a className="portfolio-link" target="_blank" rel="noopener noreferrer" href="http://www.khoipham.net">
 				khoipham.net
